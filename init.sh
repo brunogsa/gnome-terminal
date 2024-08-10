021af56cce7953043aa7f4e1ccfb3cd22947f7d5
Bruno Agostini <brunogsa.92@gmail.com>
2024-08-10 13:18:09 -0300

    adding dotfiles for gnome-terminal to restore it in a automated way later on

--- init.sh
+++ init.sh
@@ -3,3 +3,8 @@ sudo apt-get install -y gnome-terminal
 sudo apt-get install -y fonts-inconsolata
 
 echo 'export PS1="[\[$(tput bold)\]\t\[$(tput sgr0)\]] \u\[$(tput bold)\] \[$(tput sgr0)\]\w \[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;97m\]\\$\[$(tput sgr0)\] "' >> ~/.bashrc
+
+# Used to import/export its conf
+sudo apt install dconf-cli
+
+dconf load /org/gnome/terminal/ < ./gnome-terminal-settings.dconf
